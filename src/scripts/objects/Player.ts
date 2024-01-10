89a8526d7eeff46e16cc2ca2ef056cc320dd3613
aurelian.spod@gmail.com <aurelian.spod@gmail.com>
2024-01-10 23:53:55 +0100

    feat: Added arrow support

--- src/scripts/objects/Player.ts
+++ src/scripts/objects/Player.ts
@@ -15,7 +15,7 @@ export class Player {
     this.height = 16;
     this.size = 16;
     this.vel = { x: 0.15, y: 0.15 };
-    
+
     this.position.set(App.app.screen.width / 2 - this.width, App.app.screen.height / 2 - this.height);
     this.setupKeyboardEvents();
   }
@@ -28,28 +28,30 @@ export class Player {
 
   handleKeyPress(event) {
     const keyState = event.type === 'keydown' ? 1 : 0;
-
     switch (event.key.toLowerCase()) {
       case 'w':
+      case 'arrowup':
         this.keyState['w'] = keyState;
         break;
       case 's':
+      case 'arrowdown':
         this.keyState['s'] = keyState;
         break;
       case 'a':
+      case 'arrowleft':
         this.keyState['a'] = keyState;
         break;
       case 'd':
+      case 'arrowright':
         this.keyState['d'] = keyState;
         break;
     }
-    
   }
 
   createMe() {
     const me = new PIXI.Graphics()
     me.beginFill(0xffffff);
-    me.drawCircle(30, 30, 30);
+    me.drawCircle(me.width, me.height, 16);
     me.endFill();
     me.x = this.position.x;
     me.y = this.position.y;
@@ -57,17 +59,16 @@ export class Player {
   }
 
   update() {
-    console.log("@'''''''''''''''''''''''''''''''", this.position)
-    if (this.keyState['w']) {
+    if (this.keyState['w'] || this.keyState['ArrowUp']) {
       this.position.y -= 10;
     }
-    if (this.keyState['s']) {
+    if (this.keyState['s'] || this.keyState['ArrowDown']) {
       this.position.y += 10;
     }
-    if (this.keyState['a']) {
+    if (this.keyState['a'] || this.keyState['ArrowLeft']) {
       this.position.x -= 10;
     }
-    if (this.keyState['d']) {
+    if (this.keyState['d'] || this.keyState['ArrowRight']) {
       this.position.x += 10;
     }
     this.me.position.copyFrom(this.position);
